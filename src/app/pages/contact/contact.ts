83ce6b5f5e23cf3a385fad758c276ccfcca6f3ba
rony.assaf <rony.assaf101@hotmail.com>
2026-02-10 14:09:02 +0200

    added changes in the style and made the website more alive

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Navbar } from '../../componenets/navbar/navbar';
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink, Navbar],
+  imports: [Navbar],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
